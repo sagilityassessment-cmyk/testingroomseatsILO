0e08b1cff19097b3d569ec6a2f8137874989366a
sagilityph <sagilityassessment@gmail.com>
2026-07-11 16:33:39 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCal6UJkUbd4fGkqkN5iIYwmKpSBir7CPY",
   authDomain: "testing-room-iloilo.firebaseapp.com",
   databaseURL: "https://testing-room-iloilo-default-rtdb.asia-southeast1.firebasedatabase.app",
